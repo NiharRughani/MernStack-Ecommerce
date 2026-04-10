65326db889857fc8ff13eff3a3b4086caebcfaa2
NiharRughani <niharrughani30@gmail.com>
2026-04-10 18:32:38 +0530

    Refactor server.js to use environment variables

--- server/server.js
+++ server/server.js
@@ -1,18 +1,16 @@
+import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import userRouter from "./Routes/userRouter.js";
 import productRouter from "./Routes/productRouter.js";
-
 import cartRouter from "./Routes/cartRouter.js";
-
 import addressRouter from "./Routes/addressRouter.js";
-
 import paymentRouter from "./Routes/paymentRouter.js";
 
 const app = express();
 
-const port = 1000;
+const port = process.env.PORT;
 
 app.use(
   cors({
@@ -23,39 +21,23 @@ app.use(
 );
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
-//userrouter
-
 app.use("/api/user", userRouter);
-
-//Productrouter
-
 app.use("/api/product", productRouter);
-
-//cartrouter
-
 app.use("/api/cart", cartRouter);
-
-//addressrouter
-
 app.use("/api/address", addressRouter);
-
 app.use("/api/payment", paymentRouter);
 
 mongoose
-  .connect(
-    "mongodb+srv://niharrughani30_db_user:database1@cluster0.immgjwi.mongodb.net/",
-    { dbName: "MERN_PROJECT" },
-  )
+  .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("mongoDb connected succesfully");
+    console.log("MongoDB connected successfully");
   })
   .catch((err) => {
     console.log(err);
   });
 
 app.listen(port, () => {
-  console.log(`server is started url is http://localhost:${port}`);
+  console.log(`Server started on port ${port}`);
 });
